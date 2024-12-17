e7c836388e3345493c4061fececed91e5af9b81e
Amanda Isufi <amaisu18@htl-shkoder.com>
2024-12-17 09:56:10 +0100

    Divide Function was created

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -14,6 +14,13 @@ public class Calculator {
     public int multiply(int a, int b){
         return a*b;
     }
+    public int divide(int a, int b){
+        if (b==0) {
+            throw new IllegalArgumentException();
+        } else {
+            return a / b;
+        }
+    }
 }
 
 
