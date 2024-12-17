180145fce2436ba68fdd343faa8fe9a089421b3f
Ergi Dibra <ergdib18@htl-shkoder.com>
2024-12-17 10:17:48 +0100

    Divide

--- src/main/java/org/example/CalculatorTest.java
+++ src/main/java/org/example/CalculatorTest.java
@@ -2,17 +2,25 @@ package org.example;
 
 public class CalculatorTest {
 
-     public int add(int a, int b) {
+    public int add(int a, int b) {
         return a + b;
     }
 
-     public int substract(int a, int b) {
+    public int substract(int a, int b) {
         return a - b;
     }
 
-    public int multiply(int a, int b){
-        return a*b;
+    public int multiply(int a, int b) {
+        return a * b;
     }
 
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException();
+        } else {
+            return a / b;
+        }
 
+
+    }
 }
