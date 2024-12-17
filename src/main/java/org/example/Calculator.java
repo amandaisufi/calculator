370af2ea4d7c42e38119f5dd69065929bc915640
Amanda Isufi <amaisu18@htl-shkoder.com>
2024-12-17 09:54:26 +0100

    Add Function was created

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,23 +1,7 @@
 package org.example;
 
 public class Calculator {
-    public int add(int a, int b){
+    public int add(int a, int b) {
         return a + b;
     }
-
-    public int subtract(int a, int b){
-        return a - b;
-    }
-
-    public int multiply(int a, int b){
-        return a*b;
-    }
-    public int divide(int a, int b){
-        if (b==0) {
-            throw new IllegalArgumentException();
-        } else {
-            return a / b;
-        }
-    }
 }
-
